c8e2d2c92e3b03286ef180495030cc560e25eaea
kay-natsumi <kny112@hotmail.com>
2021-03-09 18:39:14 +0900

    update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-var theButton = document.getElementById("portfolio");
+ ///var theButton = document.getElementById("portfolio");
 
-theButton.onclick = function () {
- var password = prompt("Please enter the password.");
-    if (password==="pugs!2020"){
-        window.open("files/Portfolio2020.pdf","_blank");
-    } else{
-      password = prompt("oops! wrong password");
-    }
-}
+ ///theButton.onclick = function () {
+  ///var password = prompt("Please enter the password.");
+  ///   if (password==="pugs!2020"){
+  ///       window.open("files/Portfolio2020.pdf","_blank");
+  ///   } else{
+ ///     password = prompt("oops! wrong password");
+  ///   }
+ ///}
 
 
 $(function() {
